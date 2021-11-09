ad4416e7b88267d0813ba13c17b0838c8048a2c1
Pau <73888436+PauRuiz19@users.noreply.github.com>
2021-11-09 16:36:44 +0100

    Implementacion pantalla Login con hibernate
    Comprobacion login usuario correcto e incorrecto e implementacion de pantalla recordar contraseña

---
+++ Hib_Kahoot/src/main/java/errorHandling/ErrorHandler.java
@@ -0,0 +1,26 @@
+package errorHandling;
+
+public class ErrorHandler {
+	
+	
+	public ErrorHandler(String errorMessage) {
+		handleException(errorMessage);
+	}
+	
+	public ErrorHandler(Exception e, String errorMessage) {
+		handleException(e, errorMessage);
+	}
+	
+	//Checked Exception handler
+	public void handleException(Exception e, String errorMessage) {
+		String className = e.getClass().getName().toString();
+		
+		System.out.println(className + "\n" + errorMessage);
+	}
+	
+	//Custom Exception handler
+	public void handleException(String errorMessage) {
+		System.out.println(errorMessage);			
+	}
+	
+}
